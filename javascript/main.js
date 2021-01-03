8bc1c445cb8396fcfb70f1e033358b2b5140b87b
jamestheengineer <jamestheengineer@gmail.com>
2021-01-03 11:46:43 -0500

    Basic types

--- javascript/main.js
+++ javascript/main.js
@@ -1,3 +1,19 @@
-console.log('Hello World');
-console.error('This is an error');
-console.warn('This is an warning');
+// var-nah don't use, let, const
+let age = 30; 
+age = 31 // const will throw error
+
+console.log(age);
+
+// Use const unless you already know you are going to change it
+// const must be initialized
+
+// Strings, Numbers, Boolean, null, undefined, Symbol
+const name = 'John'; // semi-colons aren't mandatory, but common
+const age2 = 30;
+const rating = 4.5;
+const isCool = true;
+const x = null;
+const y = undefined;
+let z; // undefined
+ 
+console.log(typeof name);
