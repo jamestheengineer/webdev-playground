fa22e6b8881e32c1d76c10d1dbb51df5e99d8ed2
jamestheengineer <jamestheengineer@gmail.com>
2021-01-06 22:51:26 -0500

    Arrays

--- javascript/main.js
+++ javascript/main.js
@@ -28,3 +28,15 @@ console.log(s.length);
 console.log(s.toUpperCase());
 console.log(s.substring(0,5).toUpperCase());
 console.log(s.split(', '));
+
+// Arrays
+/* Multi
+line 
+comment */
+const numbers = new Array(1,2,3,4,5);
+console.log(numbers);
+
+const fruits = ['apples', 'oranges', 'pears', 10, true];
+console.log(fruits);
+
+
