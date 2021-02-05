aa1bf1e30e442c791950890c464ba371a80ce98b
jamestheengineer <jamestheengineer@gmail.com>
2021-02-05 18:22:55 -0500

    Tasks component

---
+++ react-traversy/react-task-tracker/src/components/Tasks.js
@@ -0,0 +1,31 @@
+const tasks = [
+    {
+        id: 1,
+        text: 'Doctors Appointment',
+        day: 'Feb 5th at 2:30pm',
+        reminder: true,
+    },
+    {
+        id: 1,
+        text: 'Meeting at Schoo',
+        day: 'Feb 6th at 1:30pm',
+        reminder: true,
+    },
+    {
+        id: 1,
+        text: 'Food Shopping',
+        day: 'Feb 7th at 2:30pm',
+        reminder: false,
+    }
+]
+
+
+const Tasks = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default Tasks
