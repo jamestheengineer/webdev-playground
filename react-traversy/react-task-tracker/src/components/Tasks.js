743d4b24695f35a6a15338d310a5d5c52231b8b6
jamestheengineer <jamestheengineer@gmail.com>
2021-02-07 17:18:46 -0500

    Adding some tasks

--- react-traversy/react-task-tracker/src/components/Tasks.js
+++ react-traversy/react-task-tracker/src/components/Tasks.js
@@ -6,13 +6,13 @@ const tasks = [
         reminder: true,
     },
     {
-        id: 1,
+        id: 2,
         text: 'Meeting at Schoo',
         day: 'Feb 6th at 1:30pm',
         reminder: true,
     },
     {
-        id: 1,
+        id: 3,
         text: 'Food Shopping',
         day: 'Feb 7th at 2:30pm',
         reminder: false,
@@ -22,9 +22,11 @@ const tasks = [
 
 const Tasks = () => {
     return (
-        <div>
-            
-        </div>
+        <>
+          {tasks.map((task) => (
+              <h3 key={task.id}>{task.text}</h3>
+          ))}  
+        </>
     )
 }
 
