83200847e807eca715d76c9880527c77d807d004
jamestheengineer <jamestheengineer@gmail.com>
2021-02-23 19:05:26 -0500

    Update status

--- react-traversy/react-task-tracker/src/App.js
+++ react-traversy/react-task-tracker/src/App.js
@@ -24,6 +24,13 @@ const App = () => {
     return data
   }
 
+  const fetchTask = async (id) => {
+    const res = await fetch(`http://localhost:5000/tasks/${id}`)
+    const data = await res.json()
+
+    return data
+  }
+
   // Add task
   const addTask = async (task) => {
     // const id = Math.floor(Math.random() * 10000) + 1
@@ -54,7 +61,19 @@ const App = () => {
   }
 
   // Toggle reminder
-  const toggleReminder = (id) => {
+  const toggleReminder = async (id) => {
+    const taskToToggle = await fetchTask(id)
+    const updTask = { ...taskToToggle, reminder: !taskToToggle.reminder }
+    const res = await fetch(`http://localhost:5000/tasks/${id}`, {
+      method: 'PUT',
+      headers: {
+        'Content-type': 'application/json'
+      },
+      body: JSON.stringify(updTask)
+    })
+
+    const data = await res.json()
+
     setTasks(tasks.map((task) => task.id === id
       ? { ...task, reminder: !task.reminder } : task))
   }
