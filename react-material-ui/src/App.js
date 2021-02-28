3155fee197a8d45fae58b0b9ba917fda64239a38
jamestheengineer <jamestheengineer@gmail.com>
2021-02-28 18:53:35 -0500

    Last material ui step

--- react-material-ui/src/App.js
+++ react-material-ui/src/App.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react'
 import NavBar from './components/NavBar'
+import Demo from './components/Demo'
+import { Viewer } from "resium";
+
 class App extends Component {
   render() {
     return (
       <div>
-        <NavBar />
+        <Viewer />
       </div>
     )
   }
