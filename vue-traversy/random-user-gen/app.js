07fb08e52cd5e18e5ed9207aa8671414ce09e514
jamestheengineer <jamestheengineer@gmail.com>
2021-03-01 17:26:33 -0500

    Random user example

--- vue-traversy/random-user-gen/app.js
+++ vue-traversy/random-user-gen/app.js
@@ -1,5 +1,13 @@
 const app = Vue.createApp({
-    template: '<h1>Hello World</h1>'
+    data() {
+        return {
+            firstName: 'James',
+            lastName: 'Doe',
+            email: 'james@gmail.com',
+            gender: 'male',
+            picture: 'https://randomuser.me/api/portraits/men/10.jpg',
+        }
+    }
 })
 
 app.mount('#app')
