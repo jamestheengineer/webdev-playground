c62ae216530131a6dd882271b254b0a24129ca05
jamestheengineer <jamestheengineer@gmail.com>
2021-04-06 19:48:14 -0400

    Add router

--- vue-traversy/vue-task-tracker/vue-crash-2021/src/App.vue
+++ vue-traversy/vue-task-tracker/vue-crash-2021/src/App.vue
@@ -5,6 +5,7 @@
     />
     <div v-show="showAddTask"><AddTask @add-task="addTask" /></div>
     <Tasks @toggle-reminder="toggleReminder" @delete-task="deleteTask" :tasks="tasks" />
+    <router-view></router-view>
     <Footer />
   </div>
 </template>
