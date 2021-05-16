3d8531efb463d371b9c140c1f13baae899a098be
jamestheengineer <jamestheengineer@gmail.com>
2021-05-15 20:17:36 -0400

    Email checker

--- form-validator/script.js
+++ form-validator/script.js
@@ -17,6 +17,15 @@ function showSuccess(input) {
   formControl.className = 'form-control success';
 }
 
+function isValidEmail(email) {
+  const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  if (re.test(input.value.trim())) {
+    showSuccess(input);
+  } else {
+    showError(input, 'Email is not valid');
+  }
+}
+
 // Event listeners
 form.addEventListener('submit', function(e) {
   e.preventDefault();
