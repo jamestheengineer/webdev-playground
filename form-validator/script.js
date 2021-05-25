c70ea00e66848e169cc56f806a3681ed47a03b52
jamestheengineer <jamestheengineer@gmail.com>
2021-05-25 18:23:25 -0400

    Wrap up validation

--- form-validator/script.js
+++ form-validator/script.js
@@ -21,7 +21,7 @@ function checkEmail(input) {
   const re =
     /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
-  if (re.test(input.value)) {
+  if (re.test(input.value.trim())) {
     showSuccess(input);
   } else {
     showError(input, 'Email is not valid');
@@ -56,6 +56,13 @@ function checkLength(input, min, max) {
   }
 }
 
+// Check passwords
+function checkPasswordsMatch(input1, input2) {
+  if (input1.value !== input2.value) {
+    showError(input2, 'Passwords do not match');
+  }
+}
+
 // Get fieldname
 function getFieldName(input) {
   return input.id.charAt(0).toUpperCase() + input.id.slice(1);
@@ -69,4 +76,5 @@ form.addEventListener('submit', function (e) {
   checkLength(username, 3, 15);
   checkLength(password, 6, 20);
   checkEmail(email);
+  checkPasswordsMatch(password, password2);
 });
