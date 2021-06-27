ee19d17dc1fb0adf241f94b03cf654f9ddb20c5b
jamestheengineer <jamestheengineer@gmail.com>
2021-06-27 10:35:29 -0400

    Javascript

--- exchange-rate/script.js
+++ exchange-rate/script.js
@@ -1,7 +1,23 @@
+const currencyEl_one = document.getElementById('currency-one');
+const amountEl_one = document.getElementById('amount-one');
+const currencyEl_two = document.getElementById('currency-two');
+const amountEl_two = document.getElementById('amount-two');
+
+const rateEl = document.getElementById('rate');
+const swap = document.getElementById('swap');
+
+// Fetch exchange rates and update the DOM
 function calculate() {
-  fetch('items.json')
-    .then((res) => res.json())
-    .then((data) => console.log(data));
+  const currency_one = currencyEl_one.value;
+  const currency_two = currencyEl_two.value;
+
+  console.log(currency_one, currency_two);
 }
+
+// Even listeners
+currencyEl_one.addEventListener('change', calculate);
+amountEl_one.addEventListener('input', calculate);
+currencyEl_two.addEventListener('change', calculate);
+amountEl_two.addEventListener('input', calculate);
 
 calculate();
