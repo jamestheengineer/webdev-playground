e50bcdb661f231e4bd6f6001eb22974669e91243
jamestheengineer <jamestheengineer@gmail.com>
2021-06-28 18:24:26 -0400

    Wrap up project

--- exchange-rate/script.js
+++ exchange-rate/script.js
@@ -11,7 +11,15 @@ function calculate() {
   const currency_one = currencyEl_one.value;
   const currency_two = currencyEl_two.value;
 
-  console.log(currency_one, currency_two);
+  fetch(`https://api.exchangerate-api.com/v4/latest/${currency_one}`)
+    .then((res) => res.json())
+    .then((data) => {
+      // console.log(data);
+      const rate = data.rates[currency_two];
+      rateEl.innerText = `1 ${currency_one} = ${rate} ${currency_two}`;
+
+      amountEl_two.value = (amountEl_one.value * rate).toFixed(2);
+    });
 }
 
 // Even listeners
@@ -19,5 +27,11 @@ currencyEl_one.addEventListener('change', calculate);
 amountEl_one.addEventListener('input', calculate);
 currencyEl_two.addEventListener('change', calculate);
 amountEl_two.addEventListener('input', calculate);
+swap.addEventListener('click', () => {
+  const temp = currencyEl_one.value;
+  currencyEl_one.value = currencyEl_two.value;
+  currencyEl_two.value = temp;
+  calculate();
+});
 
 calculate();
