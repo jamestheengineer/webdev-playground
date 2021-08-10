e739226541d16b6487ced6fc004eddb41ae67a0b
jamestheengineer <jamestheengineer@gmail.com>
2021-08-10 19:44:13 -0400

    more js

--- hangman/script.js
+++ hangman/script.js
@@ -40,10 +40,39 @@ function displayWord() {
   }
 }
 
+// Update the wrong letters
+function updateWrongLettersEl() {
+  console.log('Update wrong');
+}
+
+// Show notification
+function showNotification() {
+  notification.classList.add('show');
+
+  setTimeout(() => {
+    notification.classList.remove('show');
+  }, 2000);
+}
+
 // Keydown letter press
 window.addEventListener('keydown', (e) => {
   if (e.code >= 'KeyA' && e.code <= 'KeyZ') {
-    console.log(123);
+    const letter = e.key;
+    if (selectedWord.includes(letter)) {
+      if (!correctLetters.includes(letter)) {
+        correctLetters.push(letter);
+        displayWord();
+      } else {
+        showNotification();
+      }
+    } else {
+      if (!wrongLetters.includes(letter)) {
+        wrongLetters.push(letter);
+        updateWrongLettersEl();
+      } else {
+        showNotification();
+      }
+    }
   }
 });
 
