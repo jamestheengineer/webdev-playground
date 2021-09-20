525916a1644375173f5731df5642299fe073211f
jamestheengineer <jamestheengineer@gmail.com>
2021-09-20 17:25:43 -0400

    Finish js functions

--- typing-game/script.js
+++ typing-game/script.js
@@ -45,3 +45,40 @@ let time = 10;
 function getRandomWord() {
   return words[Math.floor(Math.random() * words.length)];
 }
+
+// Add word to DOM
+function addWordToDOM() {
+  randomWord = getRandomWord();
+  word.innerHTML = randomWord;
+}
+
+// Update score
+function updateScore() {
+  score++;
+  scoreEl.innerHTML = score;
+}
+
+// Update time
+function updateTime() {
+  time--;
+  timeEl.innerHTML = time + 's';
+
+  if (time === 0) {
+    clearInterval(timeInterval);
+    // end game
+    gameOver();
+  }
+}
+
+// Game over, show end screen
+function gameOver() {
+  endgameEl.innerHTML = `
+    <h1>Time ran out</h1>
+    <p>Your final score is ${score}</p>
+    <button onclick="location.reload()">Reload</button>
+  `;
+
+  endgameEl.style.display = 'flex';
+}
+
+addWordToDOM();
