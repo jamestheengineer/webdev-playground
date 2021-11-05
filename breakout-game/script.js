07aed49214d2df5424288c55591d66d1d691d36e
jamestheengineer <jamestheengineer@gmail.com>
2021-11-05 18:35:49 -0400

    Increase score

--- breakout-game/script.js
+++ breakout-game/script.js
@@ -143,11 +143,33 @@ function moveBall() {
           ball.dy *= -1;
           brick.visible = false;
 
-          //increaseScore();
+          increaseScore();
         }
       }
     });
   });
+
+  // Increase score
+  function increaseScore() {
+    score++;
+
+    if (score % (brickRowCount * brickColumnCount) === 0) {
+      ball.visible = false;
+      paddle.visible = false;
+
+      //After 0.5 sec restart the game
+      setTimeout(function () {
+        showAllBricks();
+        score = 0;
+        paddle.x = canvas.width / 2 - 40;
+        paddle.y = canvas.height - 20;
+        ball.x = canvas.width / 2;
+        ball.y = canvas.height / 2;
+        ball.visible = true;
+        paddle.visible = true;
+      }, delay);
+    }
+  }
 
   // Hit bottom wall - Lose
   if (ball.y + ball.size > canvas.height) {
