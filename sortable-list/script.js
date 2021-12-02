a22799824e9ed6aa6f19ddd7bd1142f501ba14aa
jamestheengineer <jamestheengineer@gmail.com>
2021-12-02 18:43:33 -0500

    Make dragging work

--- sortable-list/script.js
+++ sortable-list/script.js
@@ -49,31 +49,40 @@ function createList() {
 }
 
 function dragStart() {
-  console.log('Event: ', 'dragstart');
-  //dragStartIndex = +this.closest('li').getAttribute('data-index');
+  // console.log('Event: ', 'dragstart');
+  dragStartIndex = +this.closest('li').getAttribute('data-index');
 }
 
 function dragEnter() {
-  console.log('Event: ', 'dragenter');
-  //this.classList.add('over');
+  // console.log('Event: ', 'dragenter');
+  this.classList.add('over');
 }
 
 function dragLeave() {
-  console.log('Event: ', 'dragleave');
-  //this.classList.remove('over');
+  // console.log('Event: ', 'dragleave');
+  this.classList.remove('over');
 }
 
 function dragOver(e) {
-  console.log('Event: ', 'dragover');
-  //e.preventDefault();
+  // console.log('Event: ', 'dragover');
+  e.preventDefault();
 }
 
 function dragDrop() {
-  console.log('Event: ', 'drop');
-  //const dragEndIndex = +this.getAttribute('data-index');
-  //swapItems(dragStartIndex, dragEndIndex);
+  // console.log('Event: ', 'drop');
+  const dragEndIndex = +this.getAttribute('data-index');
+  swapItems(dragStartIndex, dragEndIndex);
 
-  //this.classList.remove('over');
+  this.classList.remove('over');
+}
+
+// Swap list items that are drag and drop
+function swapItems(fromIndex, toIndex) {
+  const itemOne = listItems[fromIndex].querySelector('.draggable');
+  const itemTwo = listItems[toIndex].querySelector('.draggable');
+
+  listItems[fromIndex].appendChild(itemTwo);
+  listItems[toIndex].appendChild(itemOne);
 }
 
 function addEventListeners() {
@@ -92,4 +101,4 @@ function addEventListeners() {
   });
 }
 
-check.addEventListener('click', checkOrder);
+// check.addEventListener('click', checkOrder);
